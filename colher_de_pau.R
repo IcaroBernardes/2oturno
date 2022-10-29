2d0b8de7cfe00f4fbf9a74252c286d30986f7dd4
Icaro Bernardes <asaicaro@gmail.com>
2022-10-28 22:43:24 -0300

    1o commit

---
+++ colher_de_pau.R
@@ -0,0 +1,37 @@
+# 0. Controle de Pacotes e Fontes
+## Carrega bibliotecas
+library(dplyr)
+library(glue)
+library(jsonlite)
+library(lubridate)
+library(quarto)
+
+################################################################################
+# 1. Carregamento e manejo dos dados
+## Define o código do pleito
+# pleito <- 544 ### 1o turno
+pleito <- 545 ### 2o turno
+
+## Link para o json de transmissão dos resultados
+url <- glue("https://resultados.tse.jus.br/oficial/ele2022/{pleito}/dados-simplificados/br/br-c0001-e000{pleito}-r.json")
+
+## Raspa os resultados e converte a tibble (scrap)
+dados <- jsonlite::fromJSON(url, simplifyDataFrame = TRUE) %>%
+  .[["cand"]] %>% 
+  dplyr::as_tibble()
+
+## Insere as datas
+dados <- dados %>%
+  dplyr::mutate(data = Sys.time(),
+                data = lubridate::ymd_hms(data))
+
+## Une com os resultados anteriores
+passado <- readRDS("dados.RDS")
+dados <- dplyr::bind_rows(passado, dados)
+
+## Salva o que foi obtido
+saveRDS(dados, "dados.RDS")
+
+################################################################################
+# 2. Produz a página com o gif (render)
+quarto::quarto_render("index.qmd", pandoc_args = "--self-contained")
