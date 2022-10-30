73241bc87ac573949466c07cef2e65e9beac4bd5
Icaro Bernardes <asaicaro@gmail.com>
2022-10-30 17:45:10 -0300

    pct apuração

--- colher_de_pau.R
+++ colher_de_pau.R
@@ -20,10 +20,15 @@ dados <- jsonlite::fromJSON(url, simplifyDataFrame = TRUE) %>%
   .[["cand"]] %>% 
   dplyr::as_tibble()
 
+## Obtém o percentual de urnas apuradas
+progresso <- jsonlite::fromJSON(url, simplifyDataFrame = TRUE) %>%
+  .[["pst"]]
+  
 ## Insere as datas e traz para o GMT-3
 dados <- dados %>%
   dplyr::mutate(data = Sys.time(),
-                data = lubridate::ymd_hms(data)-lubridate::dhours(3))
+                data = lubridate::ymd_hms(data)-lubridate::dhours(3),
+                apuradas = progresso)
 
 ## Une com os resultados anteriores
 passado <- readRDS("dados.RDS")
